f487cccca53bda50ae2c2dc4937928378ecbe486
Maxim Galaganov <voltage@lanport.net>
2016-03-21 23:00:47 +0300

    Experiments with hooks

--- dbapi.lua
+++ dbapi.lua
@@ -1,28 +1,43 @@
 #!/usb/bin/env tarantool
 
-box.cfg {}
+box.cfg {
+    log_level = 10,
+}
 
+local log = require('log')
 local conn = require('mysql').connect({ host = localhost, user = 'root', password = '11', db = 'tempdb' })
 
 --
 
-local function get_forums_handler(self)
-    if self.method ~= 'GET' then
-        return { status = 504 }
-    end
+local function get_forums_handler(req)
+    log.info('What the hell is going on with our equipment?')
 
     local response_data = {
         code = 0,
-        response = conn:execute('select * from forum where 1')
+        response = {
+            hello = 'World!'
+        }
     }
 
-    return self:render({ json = response_data })
+--    return response_data
+    return req:render({ json = response_data })
 end
 
+local function hook_before_routes(self, req)
+    log.info('Halo, Welt!')
+    log.info(req)
+end
+
+local function hook_after_dispatch(req, resp)
+    log.info('Auf wieder sehen, Welt!')
+    resp.body = 'Stinky gremlin stole my json!'
+end
 --
 
 local server = require('http.server').new('*', 8081)
 
-server:route({ path = '/' }, get_forums_handler)
+server:route({ path = '/', method = 'GET' }, get_forums_handler)
+server:hook('before_routes', hook_before_routes)
+server:hook('after_dispatch', hook_after_dispatch)
 
 server:start()
