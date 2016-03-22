87f998850dbbb719a4488de6678259acb9ab5646
Maxim Galaganov <voltage@lanport.net>
2016-03-22 13:08:55 +0300

    Implement sample insert transaction

--- dbapi.lua
+++ dbapi.lua
@@ -9,27 +9,40 @@ local conn = require('mysql').connect({ host = localhost, user = 'root', passwor
 
 --
 
-local function get_posts_handler(json_request)
-    log.info('_handler')
-    return {
-        code = 0,
-        request = json_request,
-        response = conn:execute('select * from post where 1')
-    }
+local function api_response(response_code, response_data)
+    return { code = response_code, response = response_data }
+end
+
+--
+
+local function create_forum(json)
+    local inserted_item = nil
+
+    conn:begin()
+    local result = conn:execute('insert into forum (name, short_name, user) values (?, ?, ?)', json.name, json.short_name, json.user)
+    if result ~= nil then
+        inserted_item = conn:execute('select * from forum where id = last_insert_id()')
+        conn:commit()
+    else
+        conn:rollback()
+    end
+
+    return api_response(0, inserted_item)
 end
 
 --
 
 local server = require('http.server').new('*', 8081)
 
-server:route({ path = '/db/api/', method = 'GET' }, get_posts_handler)
+server:route({ path = '/db/api/', method = 'POST' }, create_forum)
 
 server:hook('before_dispatch', function(self, request)
-    log.info('_hook: before_dispatch')
-    return pcall(request:json)
+    local json = nil
+    local status, err = pcall(function() json = request:json() end)
+    -- todo
+    return json
 end)
 server:hook('after_dispatch', function(self, request, request_override, response_data)
-    log.info('_hook: after_dispatch')
     return request:render({ json = response_data })
 end)
 
