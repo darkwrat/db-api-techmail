5f7c6813f5b961579ab669fa91088e0a1bcc87e9
Maxim Galaganov <voltage@lanport.net>
2016-03-23 04:28:15 +0300

    Impl. after_handler_error hook, raise true, test impl. user create, misc

--- dbapi.lua
+++ dbapi.lua
@@ -5,7 +5,7 @@ box.cfg {
 }
 
 local log = require('log')
-local conn = require('mysql').connect({ host = localhost, user = 'root', password = '11', db = 'tempdb' })
+local conn = require('mysql').connect({ host = localhost, user = 'root', password = '11', db = 'tempdb', raise = true })
 local server = require('http.server').new('*', 8081)
 
 -- -- --
@@ -22,10 +22,17 @@ local function keys_present(obj, keys)
     return true
 end
 
-local ResponseCode = { Ok = 1, NotFound = 2, BadRequest = 3, MeaninglessRequest = 4, UnknownError = 5, UserExists = 6 }
+local ResultCode = { Ok = 1, NotFound = 2, BadRequest = 3, MeaninglessRequest = 4, UnknownError = 5, UserExists = 6 }
 
-local function api_response(response_code, response_data)
+local function create_response(response_code, response_data)
     return { code = response_code, response = response_data }
+end
+
+local function single_value(obj)
+    if type(obj) ~= 'table' or table.getn(obj) ~= 1 then
+        return nil
+    end
+    return obj[1]
 end
 
 -- -- --
@@ -34,71 +41,26 @@ end
 local api_common_clear -- https://github.com/andyudina/technopark-db-api/blob/master/doc/clear.md
 local api_common_status -- https://github.com/andyudina/technopark-db-api/blob/master/doc/status.md
 
+api_common_clear = function(args)
+    conn:begin()
+    for _, v in pairs({ 'post', 'thread', 'forum', 'user' }) do
+        conn:execute('truncate table ' .. v)
+    end
+    conn:commit()
+    return create_response(ResultCode.Ok, 'OK')
+end
+
+-- todo: избавиться от count(*)
+api_common_status = function(args)
+    local result = {}
+    for _, v in pairs({ 'post', 'thread', 'forum', 'user' }) do
+        result[v] = single_value(conn:execute('select count(*) as nrows from ' .. v)).nrows
+    end
+    return create_response(ResultCode.Ok, result)
+end
+
 server:route({ path = '/db/api/clear', method = 'POST' }, api_common_clear)
 server:route({ path = '/db/api/status', method = 'GET' }, api_common_status)
-
--- forum
-local api_forum_create -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/create.md
-local api_forum_details -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/details.md
-local api_forum_list_posts -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/listPosts.md
-local api_forum_list_threads -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/listThreads.md
-local api_forum_list_users -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/listUsers.md
-
-api_forum_create = function(r)
-    if not keys_present(r.json, { 'name', 'short_name', 'user' }) then
-        return api_response(ResponseCode.MeaninglessRequest, {})
-    end
-
-    conn:begin()
-    local result = conn:execute('insert into forum (name, short_name, user) values (?, ?, ?)',
-        r.json.name, r.json.short_name, r.json.user)
-    if result then
-        local inserted_item = conn:execute('select * from forum where id = last_insert_id()')
-        conn:commit()
-        if inserted_item then
-            return api_response(ResponseCode.Ok, inserted_item)
-        end
-    end
-
-    conn:rollback()
-    return api_response(ResponseCode.UnknownError, {})
-end
-
-api_forum_details = function(r)
-    if not keys_present(r.query_params, { 'forum' }) then
-        return api_response(ResponseCode.MeaninglessRequest, {})
-    end
-
-    local forum = conn:execute('select * from forum where short_name = ?', r.query_params.forum)
-    if forum then
-        return api_response(ResponseCode.Ok, forum)
-    end
-
-    return api_response(ResponseCode.UnknownError, {})
-end
-
-server:route({ path = '/db/api/forum/create', method = 'POST' }, api_forum_create)
-server:route({ path = '/db/api/forum/details', method = 'GET' }, api_forum_details)
---server:route({ path = '/db/api/forum/listPosts', method = 'GET' }, api_forum_list_posts)
---server:route({ path = '/db/api/forum/listThreads', method = 'GET' }, api_forum_list_threads)
---server:route({ path = '/db/api/forum/listUsers', method = 'GET' }, api_forum_list_users)
-
--- post
-local api_post_create -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/create.md
-local api_post_details -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/details.md
-local api_post_list -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/list.md
-local api_post_remove -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/remove.md
-local api_post_restore -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/restore.md
-local api_post_update -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/update.md
-local api_post_vote -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/vote.md
-
---server:route({ path = '/db/api/post/create', method = 'POST' }, api_post_create)
---server:route({ path = '/db/api/post/details', method = 'GET' }, api_post_details)
---server:route({ path = '/db/api/post/list', method = 'GET' }, api_post_list)
---server:route({ path = '/db/api/post/remove', method = 'POST' }, api_post_remove)
---server:route({ path = '/db/api/post/restore', method = 'POST' }, api_post_restore)
---server:route({ path = '/db/api/post/update', method = 'POST' }, api_post_update)
---server:route({ path = '/db/api/post/vote', method = 'POST' }, api_post_vote)
 
 -- user
 local api_user_create -- https://github.com/andyudina/technopark-db-api/blob/master/doc/user/create.md
@@ -110,7 +72,27 @@ local api_user_list_posts -- https://github.com/andyudina/technopark-db-api/blob
 local api_user_unfollow -- https://github.com/andyudina/technopark-db-api/blob/master/doc/user/unfollow.md
 local api_user_update_profile -- https://github.com/andyudina/technopark-db-api/blob/master/doc/user/updateProfile.md
 
---server:route({ path = '/db/api/user/create', method = 'POST' }, api_user_create)
+api_user_create = function(args)
+    if not keys_present(args.json, { 'username', 'about', 'name', 'email' }) then
+        return create_response(ResultCode.MeaninglessRequest, {})
+    end
+
+    conn:begin()
+    if single_value(conn:execute('select 1 from user where email = ? limit 1', args.json.email)) then
+        return create_response(ResultCode.UserExists, {})
+    end
+    conn:execute('insert into user(username, about, name, email) values (?, ?, ?, ?)',
+        args.json.username, args.json.about, args.json.name, args.json.email)
+    local inserted_id = single_value(conn:execute('select last_insert_id() as x')).x
+    conn:commit()
+    if toboolean(args.json.isAnonymous) then
+        conn:execute('update user set isAnonymous = 1 where id = ?',
+            args.json.isAnonymous, inserted_id)
+    end
+    return create_response(ResultCode.Ok, single_value(conn:execute('select * from user where id = ?', inserted_id)))
+end
+
+server:route({ path = '/db/api/user/create', method = 'POST' }, api_user_create)
 --server:route({ path = '/db/api/user/details', method = 'GET' }, api_user_details)
 --server:route({ path = '/db/api/user/follow', method = 'POST' }, api_user_follow)
 --server:route({ path = '/db/api/user/listFollowers', method = 'GET' }, api_user_list_followers)
@@ -118,6 +100,41 @@ local api_user_update_profile -- https://github.com/andyudina/technopark-db-api/
 --server:route({ path = '/db/api/user/listPosts', method = 'GET' }, api_user_list_posts)
 --server:route({ path = '/db/api/user/unfollow', method = 'POST' }, api_user_unfollow)
 --server:route({ path = '/db/api/user/updateProfile', method = 'POST' }, api_user_update_profile)
+
+-- forum
+local api_forum_create -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/create.md
+local api_forum_details -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/details.md
+local api_forum_list_posts -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/listPosts.md
+local api_forum_list_threads -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/listThreads.md
+local api_forum_list_users -- https://github.com/andyudina/technopark-db-api/blob/master/doc/forum/listUsers.md
+
+--api_forum_create = function(r)
+--    if not keys_present(r.json, { 'name', 'short_name', 'user' }) then
+--        return create_response(ResultCode.MeaninglessRequest, {})
+--    end
+--
+--    local forum =
+--
+--    conn:begin()
+--    local result = conn:execute('insert into forum (name, short_name, user) values (?, ?, ?)',
+--        r.json.name, r.json.short_name, r.json.user)
+--    if not result then
+--        local inserted_item = conn:execute('select * from forum where id = last_insert_id()')
+--        conn:commit()
+--        if inserted_item then
+--            return create_response(ResultCode.Ok, inserted_item)
+--        end
+--    end
+--
+--    conn:rollback()
+--    return create_response(ResultCode.UnknownError, {})
+--end
+
+--server:route({ path = '/db/api/forum/create', method = 'POST' }, api_forum_create)
+--server:route({ path = '/db/api/forum/details', method = 'GET' }, api_forum_details)
+--server:route({ path = '/db/api/forum/listPosts', method = 'GET' }, api_forum_list_posts)
+--server:route({ path = '/db/api/forum/listThreads', method = 'GET' }, api_forum_list_threads)
+--server:route({ path = '/db/api/forum/listUsers', method = 'GET' }, api_forum_list_users)
 
 -- thread
 local api_thread_close -- https://github.com/andyudina/technopark-db-api/blob/master/doc/thread/close.md
@@ -146,6 +163,23 @@ local api_thread_vote -- https://github.com/andyudina/technopark-db-api/blob/mas
 --server:route({ path = '/db/api/thread/update', method = 'POST' }, api_thread_update)
 --server:route({ path = '/db/api/thread/vote', method = 'POST' }, api_thread_vote)
 
+-- post
+local api_post_create -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/create.md
+local api_post_details -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/details.md
+local api_post_list -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/list.md
+local api_post_remove -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/remove.md
+local api_post_restore -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/restore.md
+local api_post_update -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/update.md
+local api_post_vote -- https://github.com/andyudina/technopark-db-api/blob/master/doc/post/vote.md
+
+--server:route({ path = '/db/api/post/create', method = 'POST' }, api_post_create)
+--server:route({ path = '/db/api/post/details', method = 'GET' }, api_post_details)
+--server:route({ path = '/db/api/post/list', method = 'GET' }, api_post_list)
+--server:route({ path = '/db/api/post/remove', method = 'POST' }, api_post_remove)
+--server:route({ path = '/db/api/post/restore', method = 'POST' }, api_post_restore)
+--server:route({ path = '/db/api/post/update', method = 'POST' }, api_post_update)
+--server:route({ path = '/db/api/post/vote', method = 'POST' }, api_post_vote)
+
 -- -- --
 
 server:hook('before_dispatch', function(self, request)
@@ -153,12 +187,15 @@ server:hook('before_dispatch', function(self, request)
     if request.method ~= 'GET' then
         local status, err = pcall(function() obj.json = request:json() end)
         if not obj.json then
-            obj.response = api_response(ResponseCode.BadRequest, err)
+            obj.response = create_response(ResultCode.BadRequest, err)
         end
     else
         obj.query_params = request:query_param(nil)
     end
     return obj
+end)
+server:hook('after_handler_error', function(self, request, request_override, status, err)
+    return create_response(ResultCode.UnknownError, err)
 end)
 server:hook('after_dispatch', function(self, request, request_override, response_data)
     return request:render({ json = response_data })
