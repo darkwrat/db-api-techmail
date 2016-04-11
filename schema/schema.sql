98e7cfc14546faeab886aeeef4e53ee01c13e15c
Maxim Galaganov <voltage@lanport.net>
2016-04-11 10:45:28 +0300

    Update schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,15 +1,27 @@
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `about` text,
-  `isAnonymous` tinyint(1) NOT NULL DEFAULT '0',
-  `temp` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_email_uindex` (`email`)
-) ENGINE=InnoDB;
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: tempdb
+-- ------------------------------------------------------
+-- Server version	5.5.47-0ubuntu0.14.04.1
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `forum`
+--
+
+DROP TABLE IF EXISTS `forum`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `forum` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -19,8 +31,65 @@ CREATE TABLE `forum` (
   UNIQUE KEY `forum_short_name_uindex` (`short_name`),
   KEY `forum_user_id_fk` (`user_id`),
   CONSTRAINT `forum_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_post_id` int(11) DEFAULT NULL,
+  `isApproved` tinyint(1) NOT NULL,
+  `isHighlighted` tinyint(1) NOT NULL,
+  `isEdited` tinyint(1) NOT NULL,
+  `isDeleted` tinyint(1) NOT NULL,
+  `date` datetime NOT NULL,
+  `thread_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `forum_id` int(11) NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `dislikes` int(11) NOT NULL DEFAULT '0',
+  `isSpam` tinyint(1) NOT NULL,
+  `mpath` varchar(255) DEFAULT NULL,
+  `topmost_parent_post_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `post_user_id_fk` (`user_id`),
+  KEY `post_forum_id_fk` (`forum_id`),
+  KEY `post_thread_id_fk` (`thread_id`),
+  KEY `post_post_id_fk` (`parent_post_id`),
+  CONSTRAINT `post_forum_id_fk` FOREIGN KEY (`forum_id`) REFERENCES `forum` (`id`),
+  CONSTRAINT `post_post_id_fk` FOREIGN KEY (`parent_post_id`) REFERENCES `post` (`id`),
+  CONSTRAINT `post_thread_id_fk` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`id`),
+  CONSTRAINT `post_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `temptable`
+--
+
+DROP TABLE IF EXISTS `temptable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `temptable` (
+  `a` int(11) DEFAULT NULL,
+  `b` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `thread`
+--
+
+DROP TABLE IF EXISTS `thread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `thread` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `forum_id` int(11) NOT NULL,
@@ -38,33 +107,36 @@ CREATE TABLE `thread` (
   KEY `thread_forum_id_fk` (`forum_id`),
   CONSTRAINT `thread_forum_id_fk` FOREIGN KEY (`forum_id`) REFERENCES `forum` (`id`),
   CONSTRAINT `thread_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `post` (
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `parent_post_id` int(11) DEFAULT NULL,
-  `isApproved` tinyint(1) NOT NULL,
-  `isHighlighted` tinyint(1) NOT NULL,
-  `isEdited` tinyint(1) NOT NULL,
-  `isDeleted` tinyint(1) NOT NULL,
-  `date` datetime NOT NULL,
-  `thread_id` int(11) NOT NULL,
-  `message` text NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `forum_id` int(11) NOT NULL,
-  `likes` int(11) NOT NULL DEFAULT '0',
-  `dislikes` int(11) NOT NULL DEFAULT '0',
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `about` text,
+  `isAnonymous` tinyint(1) NOT NULL DEFAULT '0',
+  `temp` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `post_user_id_fk` (`user_id`),
-  KEY `post_forum_id_fk` (`forum_id`),
-  KEY `post_thread_id_fk` (`thread_id`),
-  KEY `post_post_id_fk` (`parent_post_id`),
-  CONSTRAINT `post_forum_id_fk` FOREIGN KEY (`forum_id`) REFERENCES `forum` (`id`),
-  CONSTRAINT `post_post_id_fk` FOREIGN KEY (`parent_post_id`) REFERENCES `post` (`id`),
-  CONSTRAINT `post_thread_id_fk` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`id`),
-  CONSTRAINT `post_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB;
+  UNIQUE KEY `user_email_uindex` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `userfollow`
+--
+
+DROP TABLE IF EXISTS `userfollow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userfollow` (
   `follower_user_id` int(11) NOT NULL DEFAULT '0',
   `followed_user_id` int(11) NOT NULL DEFAULT '0',
@@ -72,8 +144,16 @@ CREATE TABLE `userfollow` (
   KEY `userfollows_user2_id_fk` (`followed_user_id`),
   CONSTRAINT `userfollows_user2_id_fk` FOREIGN KEY (`followed_user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `userfollows_user_id_fk` FOREIGN KEY (`follower_user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `usersubscription`
+--
+
+DROP TABLE IF EXISTS `usersubscription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usersubscription` (
   `user_id` int(11) NOT NULL,
   `thread_id` int(11) NOT NULL,
@@ -81,4 +161,16 @@ CREATE TABLE `usersubscription` (
   KEY `usersubscriptions_thread_id_fk` (`thread_id`),
   CONSTRAINT `usersubscriptions_thread_id_fk` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`id`),
   CONSTRAINT `usersubscriptions_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-11 10:44:38
